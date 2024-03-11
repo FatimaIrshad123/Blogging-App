583322b5397bccfb4809deb2fa6dd1ffc444a942
FatimaIrshad <bibimemoona2017@gmail.com>
2024-03-11 11:16:16 +0500

    Updated Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,8 +10,7 @@ function App() {
     <div>
       <BrowserRouter>
           <Routes>
-            <Route path="/" element={<div>Welcome</div>} />
-            <Route path="/signin" element={<Signin />} />
+            <Route path="/" element={<Signin />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/blogs" element={<Blogs />}/>
             <Route path="/createBlogs" element={<Createblogs/>}/>
